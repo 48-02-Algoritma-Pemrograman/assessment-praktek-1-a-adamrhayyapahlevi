524a0ce8ce6c1556b0704ad0a4bb29b92434a58e
adamrhayyapahlevi <adamrhayyapahlevi@gmail.com>
2024-10-24 12:25:57 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,22 @@
 public class Nomor1{
     public static void main(String[] args){
+        int jam =1;
+        int totaljam =12 ;
+        double honorlembur = 0 ;
+
+        totaljam = jam;
+        
+        if (totaljam <= 16) {
+            honorlembur = 160000 + 15000*4;
+            System.out.println("honor lembur selama 20 jam adalah" + honorlembur*totaljam );
+        }
+        else if (totaljam > 16) {
+            honorlembur = 10000*12; //perjam
+            System.out.println("honor lembur dibawah 12 jam adalah" + honorlembur*totaljam );
+            
+            
+        }
+        
 
     }
 }
