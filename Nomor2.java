2c33127c7f7fc8c45d4d524b05db79f904e251eb
adamrhayyapahlevi <adamrhayyapahlevi@gmail.com>
2024-10-24 12:40:43 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,31 @@
 public class Nomor2{
     public static void main(String[] args){
+             
+
+        Scanner scanner = new Scanner(System.in);
+        System.err.print("pilih supir : beliau,mereka,kita = "); 
+        String Karyawan = scanner.next().toUpperCase();
+        double lamatrip ;
+        double biayatripsekilo = 13000;
+
+        switch (Karyawan) {
+            case "beliau":
+                lamatrip = 4;
+                break;
+
+            case " mereka":
+                lamatrip = 5;
+                break;
+
+            case  "kita" :
+                lamatrip = 2;
+                break;
+
+        
+            default:
+
+                break;
+        }
 
     }
 }
